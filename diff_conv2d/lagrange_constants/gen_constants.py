dac77e40bea74544b854034bb90aabdbadf4d855
OlliNiemitalo <o@iki.fi>
2024-01-23 06:19:43 +0200

    Some progress with gen_constants.py

--- diff_conv2d/lagrange_constants/gen_constants.py
+++ diff_conv2d/lagrange_constants/gen_constants.py
@@ -26,13 +26,18 @@ py_d = sample_poly_derivatives(N_y, x)
 # Samples of kth first derivatives of 2-d polynomial x^(N_x - 1)y^(N_y - 1) with dimensions ("k_y", "k_x", "y", "x")
 pxy_d = px_d.reshape(1, N_x, 1, N_x)*py_d.reshape(N_y, 1, N_y, 1)
 # Flatten derivative order dimensions and flatten spatial dimensions
-pxy_d.reshape(N_y*N_x, N_y*N_x)
+pxy_d_flat = pxy_d.reshape(N_y*N_x, N_y*N_x)
+# Derivatives of 2-d polynomial at x_0, y_0
+px_0_d = sample_poly_derivatives(N_x, x_0)
+py_0_d = sample_poly_derivatives(N_y, y_0)
+pxy_0_d = px_0_d.reshape(1, N_x)*py_0_d.reshape(N_y, 1)
+pxy_0_d_flat = pxy_0_d.reshape(N_y, N_x, 1)
+#c = mp.lu_solve(pxy_d_flat, pxy_0_d_flat)
 
-# BROKEN CODE
-x = mp.arange(N)  # Sample positions (for x and y)
-z = x**(N-1) # Data, a polynomial
-A = hankel(y[0:N], y[N-1:N*2-1])  # Matrix of inputs to the prediction
-b = y[-N:]  # Prediction target vector
-c = mp.lu_solve(A, b)  # Least squares solve prediction coefficients
-return c # Round to integer
+#x = mp.arange(N)  # Sample positions (for x and y)
+#z = x**(N-1) # Data, a polynomial
+#A = hankel(y[0:N], y[N-1:N*2-1])  # Matrix of inputs to the prediction
+#b = y[-N:]  # Prediction target vector
+#c = mp.lu_solve(A, b)  # Least squares solve prediction coefficients
+#return c # Round to integer
 
